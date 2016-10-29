cd09ffd9e3ddb46a93ff3b28f55d7a3d16611d96
Ryan Sukale <ryansukale@gmail.com>
2016-10-29 12:57:01 -0700

    Added isEnterKey and getCSVWords, added some comments

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 module.exports = {
-	getQueryParams: require('./lib/getQueryParams'),
-	arrayToMap: require('./lib/arrayToMap'),
-	getQueryString: require('./lib/getQueryString'),
-	getHostNameFromUrl: require('./lib/getHostNameFromUrl'),
-	reloadPage: require('./lib/reloadPage'),
+	// Arrays
 	excludeFromList: require('./lib/excludeFromList'),
 	sortByField: require('./lib/sortByField'),
-	getYoutubeVideoId: require('./lib/getYoutubeVideoId'),
-	isEnterKey: require('./lib/isEnterKey'),
+	arrayToMap: require('./lib/arrayToMap'),
+
+	// Browser
 	injectScript: require('./lib/injectScript'),
-	loadExternalScripts: require('./lib/loadExternalScripts')
+	loadExternalScripts: require('./lib/loadExternalScripts'),
+	reloadPage: require('./lib/reloadPage'),
+
+	// Boolean Checks
+	isValidEmail: require('./lib/isValidEmail'),
+	isEnterKey: require('./lib/isEnterKey'),
+
+	// Misc
+	getCSVWords: require('./lib/getCSVWords'),
+	getHostNameFromUrl: require('./lib/getHostNameFromUrl'),
+	getQueryParams: require('./lib/getQueryParams'),
+	getQueryString: require('./lib/getQueryString'),
+	getYoutubeVideoId: require('./lib/getYoutubeVideoId')
 };
