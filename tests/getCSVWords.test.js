6b9f250d1690aa87ce5211b6746cd63e7fa8ed23
Ryan Sukale <ryansukale@gmail.com>
2017-04-17 09:36:00 -0700

    Added test and docs for getCSVWords

---
+++ tests/getCSVWords.test.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var expect = chai.expect;
+var ROOT = '../';
+
+var getCSVWords = require(ROOT + 'lib/getCSVWords');
+
+describe('getCSVWords', function() {
+  it('splits a CSV string', function () {
+    var result = getCSVWords('hello,world');
+    expect(result).to.deep.equal(['hello', 'world']);
+  });
+
+  it('splits a CSV string while trimming spaces', function () {
+    var result = getCSVWords('hello,   world ');
+    expect(result).to.deep.equal(['hello', 'world']);
+  });
+
+  it('splits a CSV string and excludes empty values', function () {
+    var result = getCSVWords(',  hello,, ,    ,world, ,');
+    expect(result).to.deep.equal(['hello', 'world']);
+  });
+});
