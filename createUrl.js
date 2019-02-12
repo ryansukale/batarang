c7ec7c597499fcad3883cde42fee935e29c5153a
Marcelo Eden <eden@3den.org>
2019-02-11 16:46:48 -0800

    fix decoding

--- createUrl.js
+++ createUrl.js
@@ -3,15 +3,27 @@ var isEmpty = require('lodash/isEmpty');
 var queryString = require('query-string');
 var lookup = {};
 
+function parseConfig(config) {
+  if (!config.encode) {
+    config.params = config.params || {};
+    config.query = config.query || {};
+
+    config.params.encode = (value) => value;
+    config.query.encode = false;
+  }
+
+  return config;
+}
+
 function createUrl(urlPattern, options, config) {
   var base;
   var params = options.params;
   var query = options.query;
-  config = config || {};
+  config = parseConfig(config || {});
 
   if (!isEmpty(params)) {
     lookup[urlPattern] = lookup[urlPattern] || pathToRegexp.compile(urlPattern);
-    console.log(config.params);
+
     base = lookup[urlPattern](params, config.params);
   } else {
     base = urlPattern;
--- tests/createUrl.test.js
+++ tests/createUrl.test.js
@@ -43,20 +43,16 @@ describe('createUrl', function () {
   });
 
   // Pending https://github.com/pillarjs/path-to-regexp/issues/177
-  xit('uses custom config for params', function () {
-    const config = {
-      params: {
-        encode: function (value) { return value; }
-      }
-    };
+  it('uses custom config for params', function () {
+    const config = { encode: false };
 
     expect(
-      createUrl('/user/:id', {
-        params: { id: ':/' },
-        query: query
+      createUrl('/user/:id(.*)', {
+        params: { id: 'foo/b$a' },
+        query: {back: 'http://back.com'}
       }, config)
     ).to.deep.equal(
-      '/user/:/?page=1&sort=name'
+      '/user/foo/b$a?back=http://back.com'
     );
   });
 });
