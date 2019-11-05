81b06c0bfae19a97ffbe9a667037b21e31425f98
Ryan Sukale <ryansukale@gmail.com>
2019-11-04 21:41:38 -0500

    Cleanup stateTracker

--- stateTracker.js
+++ stateTracker.js
@@ -1,23 +1,3 @@
-// const possibleStates = ['empty', 'full'];
-// const initialState = 'full';
-
-// function stateTracker(possibleStates, initialState) {
-//   let state = initialState || possibleStates[0];
-//   const properties = {
-//     setState: (s) => {state = s;}
-//   };
-
-//   possibleStates.map( key => {
-//       properties[key] = {
-//         enumerable: true,
-//         get: () => state === key
-//       }
-//     }
-//   )
-
-//   return properties;
-// }
-
 function stateTracker(possibleStates, initialState) {
   let state = initialState || possibleStates[0];
   const properties = {
@@ -33,26 +13,3 @@ function stateTracker(possibleStates, initialState) {
 }
 
 module.exports = stateTracker;
-
-// const context.empty;
-
-// export default stateTracker(possibleStates, initialState) {
-//   const activeState = possibleStates[initialState];
-
-//   for (const key of Object.keys(possibleStates)) {
-//     return {
-//     get() {
-//       return activeState;
-//     }
-//     setState(key) {
-
-//     }
-//   }
-
-//   return {
-//     activeState: {
-//       get activeState() => 'hello'
-//     }
-//   }
-// }
-
